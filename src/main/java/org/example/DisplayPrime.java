f02b674cf5e0475534aa99bf50ea8d83daca19d3
erikmegan <erik@tiket.com>
2024-04-12 17:35:12 +0700

    define prime method

--- src/main/java/org/example/DisplayPrime.java
+++ src/main/java/org/example/DisplayPrime.java
@@ -20,4 +20,13 @@ public class DisplayPrime {
         }
         return true;
     }
+
+    public static Boolean definePrime(int n){
+        for(int i=2;i<n;i++) {
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- src/test/java/DisplayPrimeTest.java
+++ src/test/java/DisplayPrimeTest.java
@@ -1,15 +1,23 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.example.DisplayPrime.definePrime;
 import static org.example.DisplayPrime.displayPrimes;
 
 public class DisplayPrimeTest {
 
     @Test
     void defaultSuccess(){
-        int n = 100; // Ubah nilai ini sesuai dengan kebutuhan Anda
+        int n = 100;
 
 		System.out.println("Prime numbers up to " + n + ":");
 		displayPrimes(n);
+    }
+
+    @Test
+    void definePrimeTest(){
+        int n = 13;
+        Assertions.assertEquals(true,definePrime(n));
     }
 
 }
